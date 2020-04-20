7f9fedb1f63e75710fa5520818bc84dc7cc9a889
Soraer <hylepik@163.com>
2020-04-20 23:40:43 +0800

    init project

--- Travel/src/main.js
+++ Travel/src/main.js
@@ -3,8 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastClick from 'fastclick' // 安装fastclick 解决移动端点击300毫秒延时问题
+import './assets/styles/reset.css'  //重置不同手机得默认值不同问题
+import './assets/styles/border.css'  //解决移动端1像素边框问题
 
 Vue.config.productionTip = false
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
