bc31aefb2f704612030d4816e5aea5ae35c05c25
Soraer <hylepik@163.com>
2020-04-21 00:05:18 +0800

    make home header

--- Travel/src/page/home/Home.vue
+++ Travel/src/page/home/Home.vue
@@ -1,17 +1,20 @@
 <template>
-  <div>hello world</div>
+  <div>
+    <home-header></home-header>
+  </div>
 </template>
 
 <script>
+import HomeHeader from './components/Header'
 export default {
-  name: 'HelloWorld',
-  data () {
-    return {}
+  name: 'Home',
+  components: {
+    HomeHeader
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style lang="stylus">
 
 </style>
---
+++ Travel/src/page/home/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="header">
+    <div class="header-left">返回</div>
+    <div class="header-input">内容</div>
+    <div class="header-right">城市</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="stylus" scoped>
+  .header
+    display : flex
+    line-height: .86rem
+    background : #00bcd4
+    color : #fff
+    .header-left
+      width : .64rem
+      float left
+    .header-input
+      flex: 1
+      height : .64rem
+      margin-top .12rem
+      margin-left .2rem
+      background #ffffff
+      border-radius .1rem
+    .header-right
+      width 1.24rem
+      text-align center
+      float : right
+</style>
