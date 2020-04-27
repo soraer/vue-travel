42db1331b0fff838b5b95f05dfba65895602ebe7
Soraer <hylepik@163.com>
2020-04-27 23:47:08 +0800

    finished city page view

---
+++ Travel/src/page/city/City.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <city-header></city-header>
+    <city-search></city-search>
+    <city-list></city-list>
+  </div>
+</template>
+
+<script>
+import CityHeader from "./components/Header";
+import CitySearch from "./components/Search";
+import CityList from "./components/List";
+export default {
+  name: "City",
+  components: {
+    CityHeader,
+    CitySearch,
+    CityList
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="stylus" scoped></style>
--- Travel/src/page/home/components/Header.vue
+++ Travel/src/page/home/components/Header.vue
@@ -7,48 +7,62 @@
       <span class="iconfont">&#xe632;</span>
       输入城市/景点/游玩主题
     </div>
-    <div class="header-right">
-      城市
-      <span class="iconfont arrow-icon">&#xe64a;</span> 
-    </div>
+    <router-link to="/city">
+      <div class="header-right">
+        城市
+        <span class="iconfont arrow-icon">&#xe64a;</span>
+      </div>
+    </router-link>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HomeHeader'
-}
+  name: "HomeHeader"
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="stylus" scoped>
-  @import '~styles/varibles.styl'
-  .header
-    display : flex
-    line-height: .86rem
-    background : $bgColor
-    color : #fff
-    .header-left
-      width : .64rem
-      float : left
-      .back-icon
-        font-size : .4rem
-        text-align : center
-    .header-input
-      flex : 1
-      color : #eee
-      line-height : .64rem
-      height : .64rem
-      margin-top : .12rem
-      margin-left : .2rem
-      background : #ffffff
-      border-radius : .1rem
-      padding-left : .2rem
-    .header-right
-      width : 1.24rem
-      text-align : center
-      float : right
-      .arrow-icon
-        font-size : .24rem
-        margin-left : .24rem
+@import '~styles/varibles.styl';
+
+.header {
+  display: flex;
+  line-height: 0.86rem;
+  background: $bgColor;
+  color: #fff;
+
+  .header-left {
+    width: 0.64rem;
+    float: left;
+
+    .back-icon {
+      font-size: 0.4rem;
+      text-align: center;
+    }
+  }
+
+  .header-input {
+    flex: 1;
+    color: #eee;
+    line-height: 0.64rem;
+    height: 0.64rem;
+    margin-top: 0.12rem;
+    margin-left: 0.2rem;
+    background: #ffffff;
+    border-radius: 0.1rem;
+    padding-left: 0.2rem;
+  }
+
+  .header-right {
+    width: 1.24rem;
+    text-align: center;
+    float: right;
+    color : #fff;
+    .arrow-icon {
+      font-size: 0.24rem;
+      margin-left: 0.24rem;
+    }
+  }
+}
 </style>
