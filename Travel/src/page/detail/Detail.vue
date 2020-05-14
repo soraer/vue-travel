1ec09d6cdbf4f9a53406396077d147837f60c44b
Soraer <hylepik@163.com>
2020-05-14 23:13:13 +0800

    finished header

--- Travel/src/page/detail/Detail.vue
+++ Travel/src/page/detail/Detail.vue
@@ -3,20 +3,22 @@
     <detail-banner 
                 :sightName="sightName"
                 :bannerImg="bannerImg"
-                :bannerImgs="gallaryImgs"></detail-banner>
+                :bannerImgs="gallaryImgs">
+    </detail-banner>
+    <detail-header></detail-header>
   </div>
 </template>
 
 <script>
 import DetailBanner from './components/Banner'
-// import DetailHeader from './components/Header'
+import DetailHeader from './components/Header'
 // import DetailList from './components/List'
 import axios from 'axios'
 export default {
   name: 'Detail',
   components: {
     DetailBanner,
-    // DetailHeader,
+    DetailHeader,
     // DetailList
   },
   data () {
